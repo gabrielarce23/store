ea13d3b540c755aabc6cfffa9362aa9818b3d33f
LAPTOP-A1BNJBOD\sqquid <gabriel@sqquid.com>
2021-07-18 22:22:46 -0300

    Cambios para generar QR dinámicamente

---
+++ static/assets/js/codigo.js
@@ -0,0 +1,17 @@
+
+const setup = () => {
+    let divs = document.querySelectorAll('.qr-img')
+    console.log(divs)
+    divs.forEach((div,i) => {
+        const str = (i + 1) + ''
+        const qrString = `${window.location.origin}/api/productos?codigo=PRCODE${str.padStart(3,'0')}`
+        if(i === 0) {
+            
+            document.getElementById('example').innerText = qrString
+        }
+        new QRCode(div,qrString)
+    })
+
+}
+
+setup()
